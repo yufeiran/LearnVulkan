0d9218356786c7583d7f120143a955f393469393
yufeiran <yufeiran1999@gmail.com>
2023-03-07 15:33:54 +0800

    完成1.2 instance

--- 1_1_Setup/main.cpp
+++ 1_1_Setup/main.cpp
@@ -1,9 +1,11 @@
+#include<glad/glad.h>
 #define GLFW_INCLUDE_VULKAN
 #include<GLFW/glfw3.h>
 
 #include<iostream>
 #include<stdexcept>
 #include<cstdlib>
+#include<vector>
 
 class HelloTriangleApplication {
 public:
@@ -17,6 +19,7 @@ public:
 	const uint32_t HEIGHT = 600;
 private:
 	GLFWwindow* window;
+	VkInstance instance;
 	void initWindow() {
 		glfwInit();
 
@@ -28,7 +31,7 @@ private:
 	}
 
 	void initVulkan() {
-
+		createInstance();
 	}
 
 	void mainLoop() {
@@ -39,8 +42,48 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 		glfwTerminate();
+	}
+
+	void createInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions( & glfwExtensionCount);
+
+		createInfo.enabledExtensionCount =glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+		std::cout << "available extensions:\n";
+		for (const auto& extension : extensions) {
+			std::cout << '\t' << extension.extensionName << '\n';
+		}
+
 	}
 };
 
